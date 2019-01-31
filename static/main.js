91e395dfdac130e7e0ba94e948c6667fa653e3ad
pg07codes <pg07codes@gmail.com>
2019-01-31 08:49:10 +0530

    qr from input done

--- static/main.js
+++ static/main.js
@@ -1,12 +1,27 @@
 
-
+let btn0=$("#urlBtn")
 let btn1=$("#currentQRbtn")
 let btn2=$("#allETQRbtn")
 let btn3=$("#allQRbtn")
 let QRcvs=$("#qrcode")
+
 let API_URL="https://api.qrserver.com/v1/create-qr-code/"
 let ALL_OPEN_URLS=[]
 let qrSize;
+
+
+function validateURL(str) {
+    let URLregex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+    if(URLregex.test(str))
+    {
+        console.log("Valid URL")
+        return true
+    }
+    else{
+        console.log("Invalid URL")
+        return false
+    }
+}
 
 chrome.tabs.query({'active':true},function(currentTab){
     ALL_OPEN_URLS.push(currentTab[0].url) 
@@ -21,6 +36,18 @@ function pushOtherTabs(){
 
     })
 }
+btn0.click(()=>{
+    QRcvs.empty()
+    let data=$("#url").val()
+    if(data.length!==0 &&  validateURL(data) ){
+        let qrSize=data.length<=90?150:(data.length<=150?220:280)
+        
+        QRcvs.append(`
+            <img id="qr" src="${API_URL}?size=${qrSize}x${qrSize}&data='${data}'" alt="QR-code">
+        `)
+    }
+    
+})
 
 btn1.click(()=>{
     QRcvs.empty()
